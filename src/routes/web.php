a33b29f61fc0e7720ea9d97e5b5438e3bb224594
karen <caremayo812@icloud.com>
2022-06-15 15:48:24 +0900

    web.phpの変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,8 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('hello', function () {
-    return '<html><body><h1>Hello</h1><p>This is sample page.
-    </p></body></html>';
-});
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>This is a sample page.</p>
+    <p>これは、サンプルで作ったページです</p>
+</body>
+</html>
+EOF;
 
+Route::get('hello', function () use ($html){
+    return $html;
+});
