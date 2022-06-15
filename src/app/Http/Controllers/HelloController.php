d9ca368bd2ad5851e2c57a19377dda5c01575d84
karen <caremayo812@icloud.com>
2022-06-15 16:26:50 +0900

    p48 複数アクションの利用　まで終了

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -4,21 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+global $head, $style, $body, $end;
+$head = '<html><head>';
+$body = '</head><body>';
+$end = '</body></html>';
+
+function tag($tag, $txt){
+    return "<{$tag}>" . $txt . "</{$tag}>";
+}
+
 class HelloController extends Controller
 {
-    public function  index()
-    {
-        return <<<EOF
+    public function  index(){
+        global $head, $style, $body, $end;
 
-        <html>
-        <head>
-            <title>Hello/Index</title>
-        </head>
-        <body>
-            <h1>Index</h1>
-            <p>これは、Helloコントローラのindexアクションです。</p>
-        </body>
-        </html>
-        EOF;
+        $html = $head . tag('title', 'Hello/Index') . $style .
+            $body
+            . tag('h1', 'Index') . tag('p', 'this is Index page')
+            . '<a href = "/hello/other">go to Other page</a>'
+            . $end;
+
+        return $html;
+    }
+
+    public function  other(){
+        global $head, $style, $body, $end;
+
+        $html = $head . tag('title', 'Hello/Other') . $style .
+            $body
+            . tag('h1', 'Other') . tag('p', 'this is Other page')
+            . $end;
+
+        return $html;
     }
 }
