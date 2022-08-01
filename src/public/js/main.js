a01f333429c94f4ef7cdcb8c44b59fdc74549396
karen <caremayo812@icloud.com>
2022-08-01 23:11:04 +0900

    クリック時の動作つけるためにとりあえずJSファイル繋げた！

---
+++ src/public/js/main.js
@@ -0,0 +1,4 @@
+console.log('できてるよーーー');
+const isCorrect = function(choice_num){
+//まだ途中
+}
--- src/resources/views/layouts/kuizy.blade.php
+++ src/resources/views/layouts/kuizy.blade.php
@@ -2,6 +2,7 @@
     <head>
         <title>@yieled('title')</title>
         <link href="https://unpkg.com/sanitize.css" rel="stylesheet"/>
+        <link rel="stylesheet" href="{{asset('/css/style.css')}}">
     </head>
 
     <body>
@@ -10,16 +11,17 @@
         <ul>
             @show
         </ul>
-            @foreach($questionWithChoices as $questionWithChoice)
+            @foreach($questionWithChoices as $question_num => $questionWithChoice)
             <img src="{{ $questionWithChoice->first()->image }}" alt="">
-            <ul>
-                @foreach($questionWithChoice as $Choice)
-                    <li>{{ $Choice->choices }}</li>
+            <ul class="choices">
+                @foreach($questionWithChoice as $choice_num => $Choice)
+                    <li onclick="isCorrect({{ $Choice->choices }})" id="choice_{{$question_num}}_{{$choice_num}}">{{ $Choice->choices }}</li>
                 @endforeach
             </ul>
             @endforeach
         <div>
             @yield('footer')
         </div>
+        <script src="{{asset('/js/main.js')}}"></script>
     </body>
-</html>
+    </html>
