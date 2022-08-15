00536a696e582dc6ddb6e7ec4ff6c58d55dced53
karen <caremayo812@icloud.com>
2022-08-15 13:33:13 +0900

    前回のレビューを受けて修正

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,7 +1,7 @@
 console.log('できてるよーーー');
 const isCorrect = function(choice_num, correct_id){
 document.getElementById(choice_num).className = 'choice_wrong';
-const correct_choice = correct_id + '_correct';
-document.getElementById(correct_choice).className = 'choice_correct';
+const correctChoice = correct_id + '_correct';
+document.getElementById(correctChoice).className = 'choice_correct';
 
 }
--- src/resources/views/kuizy/kuizy.blade.php
+++ src/resources/views/kuizy/kuizy.blade.php
@@ -1,20 +1,19 @@
 @extends('layouts.kuizy')
 
-
-        @section('head', $quiz1)
-        @section('title', $quiz1)
+        @section('head', '〇〇の難読地名クイズ')
+        @section('title', '〇〇の難読地名クイズ') <!--後で変数に -->
         @section('menubar')
-            <li><a href="{{route('Kuizy.tokyo')}}">{{$quiz1}}</a></li>
-            <li><a href="{{route('Kuizy.hiroshima')}}">{{$quiz2}}</a></li>
+            <li><a href="{{route('Kuizy.tokyo')}}">東京の難読地名クイズ</a></li>
+            <li><a href="{{route('Kuizy.hiroshima')}}">広島の難読地名クイズ</a></li>
         @endsection
         @section('question')
         @foreach($questionWithChoices as $question_num => $questionWithChoice)
         <img src="{{ $questionWithChoice->first()->image }}" alt="">
         <ul class="choices">
             @foreach($questionWithChoice as $choice_num => $Choice)
-            @if ($Choice->is_correct == 1)
+            @if ($Choice->is_correct == true)
             <li onclick="isCorrect('choice_{{$question_num}}_correct','choice_{{$question_num}}')" id="choice_{{$question_num}}_correct" class="choice">{{ $Choice->choices }}</li>
-            @elseif($Choice->is_correct == 0)
+            @elseif($Choice->is_correct == false)
             <li onclick="isCorrect('choice_{{$question_num}}_wrong{{$loop->iteration}}','choice_{{$question_num}}')" id="choice_{{$question_num}}_wrong{{$loop->iteration}}" class="choice">{{ $Choice->choices }}</li>
             @endif
             @endforeach
