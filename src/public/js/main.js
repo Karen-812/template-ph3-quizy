305561215825f1779e353df2a317ec63239ff17c
karen <caremayo812@icloud.com>
2022-08-21 11:12:12 +0900

    classList.add使った書き方に変更

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,7 +1,11 @@
-console.log('できてるよーーー');
-const isCorrect = function(choice_num, correct_id){
-document.getElementById(choice_num).className = 'choice_wrong';
-const correctChoice = correct_id + '_correct';
-document.getElementById(correctChoice).className = 'choice_correct';
-
+const isCorrect = function(choice_num, question_id){
+// choice_num：choice_{{$choice->question_id}}_correct/wrong1・２
+// question_id：choice_{{$choice->question_id}}
+document.getElementById(choice_num).classList.add('choice_wrong');
+const correctChoice = question_id + '_correct';
+const wrongChoice = question_id + '_wrong1';
+const wrongChoice2 = question_id + '_wrong2';
+document.getElementById(correctChoice).classList.add('choice_correct');
+document.getElementById(wrongChoice).classList.add = ('choice_another');
+document.getElementById(wrongChoice2).classList.add = ('choice_another');
 }
