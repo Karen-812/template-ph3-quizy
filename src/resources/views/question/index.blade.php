38cb891553790aa0b4a21f31d4875bc11815ddc5
karen <caremayo812@icloud.com>
2022-08-25 15:50:03 +0900

    ログイン機能・ハッシュ化・時間制限追加！

--- src/resources/views/question/index.blade.php
+++ src/resources/views/question/index.blade.php
@@ -7,6 +7,12 @@
 @endsection
 
 @section('question')
+@if(Auth::check())
+<p>USER: {{$user->name . '(' . $user->email . ')'}}</p>
+@else
+<p>※ログインしていません。(<a href="/login">ログイン</a>｜
+    <a href="/register">登録</a>)</p>
+@endif
 @foreach($item->questions as $question)
 <img src="{{ $question->image }}" alt="">
 <ul class="choices">
