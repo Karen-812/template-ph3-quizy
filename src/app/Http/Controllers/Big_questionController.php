22f3ce2b046c6bc7ebec1f9bef3bea460a3c8c52
karen <caremayo812@icloud.com>
2022-08-25 16:01:23 +0900

    npm入れちゃった...

--- src/app/Http/Controllers/Big_questionController.php
+++ src/app/Http/Controllers/Big_questionController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Big_question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Big_questionController extends Controller
 {
     public function index(Request $request)
     {
         $big_items = Big_question::with('questions.choices')->get();
-        return view('question.index', ['big_items' => $big_items]);
+        $user = Auth::user();
+        $param = ['big_items' => $big_items, 'user' => $user];
+        return view('question.index', $param);
     }
 }
--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 
 class HelloController extends Controller
 {
         public function index(Request $request, Response $response){
+            $user = Auth::user();
             $items = DB::select('select * from people');
-            return view('hello.index', ['items' => $items]);
+            $param = ['items' => $items, 'user' => $user];
+            return view('hello.index', $param);
         }
 }
