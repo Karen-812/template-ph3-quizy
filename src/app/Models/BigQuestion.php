558ddd6e09bcdec6eacf5361f8788ad7024da816
karen <caremayo812@icloud.com>
2022-09-29 22:35:55 +0900

    順番変更とりあえずできた！

--- src/app/Models/BigQuestion.php
+++ src/app/Models/BigQuestion.php
@@ -16,5 +16,6 @@ class BigQuestion extends Model
     }
     public $timestamps = false;
     // Unknown column 'updated_at' というエラーが出たため追加
-    protected $fillable = ['name']; 
+    protected $fillable = ['name', 'order']; 
+ 
 }
